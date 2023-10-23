868af20f7595be11906310c8e0d6bfec79b95fa3
Francis Breidenbach <fbreidenbach@gmail.com>
2023-10-23 14:24:44 -0700

    Initial Commit

---
+++ LA-Metro/LA_MetroApp.swift
@@ -0,0 +1,17 @@
+//
+//  LA_MetroApp.swift
+//  LA-Metro
+//
+//  Created by Francis Breidenbach on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct LA_MetroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
