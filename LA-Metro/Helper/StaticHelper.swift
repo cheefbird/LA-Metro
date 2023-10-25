b3d8b93efd6ada0eeeb8a3025064b7d6312dd5c2
Francis Breidenbach <fbreidenbach@gmail.com>
2023-10-25 12:50:00 -0700

    static location helper for testing

---
+++ LA-Metro/Helper/StaticHelper.swift
@@ -0,0 +1,12 @@
+//
+//  StaticHelper.swift
+//  LA-Metro
+//
+//  Created by Francis Breidenbach on 10/25/23.
+//
+
+import Foundation
+
+struct StaticHelper {
+  static let homeLocation: (Double, Double) = (34.110132, -118.204452)
+}
