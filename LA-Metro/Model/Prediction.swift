25f1327d51a281fed213136aafef926f6101b001
Francis Breidenbach <fbreidenbach@gmail.com>
2023-10-24 18:35:49 -0700

    Start on Models

---
+++ LA-Metro/Model/Prediction.swift
@@ -0,0 +1,9 @@
+//
+//  Prediction.swift
+//  LA-Metro
+//
+//  Created by Francis Breidenbach on 10/24/23.
+//
+
+import Foundation
+34.11013282198517, -118.20445242579609
---
+++ LA-Metro/Model/Stop.swift
@@ -0,0 +1,19 @@
+//
+//  Stop.swift
+//  LA-Metro
+//
+//  Created by Francis Breidenbach on 10/24/23.
+//
+
+import Foundation
+
+struct Stop: Codable {
+  
+  let id: Int
+  let name: String
+  let code: Int
+  let latitude: Double
+  let longitude: Double
+  
+  
+}
